9960cb0416e942690223b194e8b0f4cbb60074fe
Sergio Martín <sergio.mare2002@gmail.com>
2023-06-19 00:40:15 +0200

    Comprobación datos del formulario

--- js/app.js
+++ js/app.js
@@ -3,19 +3,32 @@
  */
 
 // Inicialización de variables, objetos, DOM
-const botonJugar = document.getElementById("jugar");
+const nickInput = document.getElementById("nick");
+const tamanoInput = document.getElementById("tamano");
+const formEntrada = document.getElementById("formEntrada");
 
 // Funciones de evento
-function comprobarForm () {
-    console.log("Comprobamos el formulario");
+function comprobarForm (event) {
+    // Comprobar cambios
+    if (nickInput.value.length == 0) {
+        console.log("No hay nick");
+        nickInput.focus(); // El cursor se colocará en el input automáticamente
+        event.preventDefault(); // Evitamos que se haga el submit
+        return false; // Es buena práctica que los manejadores de eventos devuelvan valores boolenaos
+    } else if (tamanoInput.value == "0") {
+        console.log("No se ha seleccionado tamaño de panel");
+        tamanoInput.focus();
+        event.preventDefault();
+        return false;
+    }
+    return true;
 }
 
-function ejecutarAccion () {
-    console.log("Ejecutar accion");
-    // Una vez ejecutada la acción asocida al evento eliminamos el controlador de eventos (ejecutarAccion) del botón para evitar que se ejecute de nuevo
-    botonJugar.removeEventListener('click', ejecutarAccion);
-}
+// Inicio de carga de eventos
 
-// Con addEventListener podemos definir varias funciones para un mismo evento en un mismo objeto
-botonJugar.addEventListener('click', comprobarForm);
-botonJugar.addEventListener('click', ejecutarAccion);
+// El evento submit es desecadenado también al pulsar ENTER dentro del formulario, a parte de dar click al botón de JUGAR de tipo submit
+formEntrada.addEventListener('submit', comprobarForm);
+
+
+
+
