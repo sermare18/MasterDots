77b58b67cd29b7ed34c89bb52443ebc4c8016f9a
Sergio Martín <sergio.mare2002@gmail.com>
2023-06-20 11:31:44 +0200

    Mensajes de error en el formulario

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@
 const nickInput = document.getElementById("nick");
 const tamanoInput = document.getElementById("tamano");
 const formEntrada = document.getElementById("formEntrada");
+const error = document.getElementById("error");
 
 // Funciones de evento
 function comprobarForm (event) {
@@ -14,11 +15,13 @@ function comprobarForm (event) {
         console.log("No hay nick");
         nickInput.focus(); // El cursor se colocará en el input automáticamente
         event.preventDefault(); // Evitamos que se haga el submit
+        error.innerText = "El campo de nick no puede estar vacío"; // Establecemos el mensaje de error
         return false; // Es buena práctica que los manejadores de eventos devuelvan valores boolenaos
     } else if (tamanoInput.value == "0") {
         console.log("No se ha seleccionado tamaño de panel");
         tamanoInput.focus();
         event.preventDefault();
+        error.innerText = "Se debe seleccionar un tamaño de panel"; // Establecemos el mensaje de error
         return false;
     }
     return true;
