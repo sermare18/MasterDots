8c69a8366ecf70cff55d091d11fd2caac434a8e4
Sergio Martín <sergio.mare2002@gmail.com>
2023-06-23 11:39:10 +0200

    Ejemplo LocalStorage

--- js/datosUsuarios.js
+++ js/datosUsuarios.js
@@ -5,9 +5,15 @@
 function datosUsuarios(nick) {
     // Almacenamos un nuevo elemento en la sesión de la forma clave: valor
     sessionStorage.setItem('nick', nick.value);
+    // A borrar en otros vídeos
+    // El localStorage no se borra al cerrar el navegador
+    localStorage.setItem('nick', nick.value);
 }
 
 function mostrarDatosUsuario() {
     let nick = sessionStorage.getItem('nick');
     console.log(nick);
+    // A borrar en otros videos
+    let nickLocal = localStorage.getItem('nick')
+    console.log(nickLocal);
 }
