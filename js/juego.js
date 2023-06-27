f5b7957ff560680321a2c17d7a44c3d691f642c4
Sergio Martín <sergio.mare2002@gmail.com>
2023-06-27 18:29:23 +0200

    Pintar el tablero de juego con JS

--- js/juego.js
+++ js/juego.js
@@ -2,6 +2,18 @@
  * JS Para el juego Masterdots
  */
 
+/**
+ * Devuelve un numero random entre 0 y max
+ * @param {Integer} max
+ * @returns {Integer} numero aleatorio entero comprendido entre 0 y max
+ */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+/**
+ * Función que rellena nick y src de avatar
+ */
 function rellenarFormularioUsuario() {
     document.getElementById("nick").value = nick;
     document.getElementById("avatarImg").src = avatarImg;
@@ -10,6 +22,15 @@ function rellenarFormularioUsuario() {
 function pintarPanelJuego() {
     document.getElementById("juego").style.gridTemplateColumns = `repeat(${tamano}, 1fr)`;
     document.getElementById("juego").style.gridTemplateRows = `repeat(${tamano}, 1fr)`;
+    // Elementos de forma automática
+    let items = "";
+    let color = ["rojo", "verde"];
+    let colorRnd = 0;
+    for (let index = 0; index < (parseInt(tamano)*parseInt(tamano)); index++) {
+        if (index % 2 > 0) colorRnd = getRandomInt(2);
+        items+=`<div class="containerItem"><div class="item ${color[colorRnd]}"></div></div>`;
+    }
+    document.getElementById("juego").innerHTML = items;
 }
 
 // Capturamos los datos del usuario
